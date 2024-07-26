5d3f19390a4d15c3243641ba42f7ce01c4d78f32
kchsu <kevin3117213@gmail.com>
2024-07-26 14:01:38 +0800

    Add: sidekiq web ui enabled

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  require "sidekiq/web"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -8,4 +10,8 @@ Rails.application.routes.draw do
   resources :subscribers, only: [:new, :create]
 
   root "subscribers#new"
+
+  BusTracker::Application.routes.draw do
+    mount Sidekiq::Web => "/sidekiq"
+  end
 end
