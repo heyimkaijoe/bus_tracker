6246991b606c16e3f7125c606fb69dad837879a9
kchsu <kevin3117213@gmail.com>
2024-07-26 15:44:38 +0800

    Modify: notif-triggering logic

--- app/jobs/track_bus_job.rb
+++ app/jobs/track_bus_job.rb
@@ -2,15 +2,25 @@ class TrackBusJob < ApplicationJob
   queue_as :default
 
   def perform(subscriber)
-    loop do
-      bus_arrival_time_data = fetch_stops_before_bus_arrival_time(subscriber.route, "Direction eq #{subscriber.route_dir}")
-      if bus_arrival_time_data["code"] = "200"
-        stops_before_bus_arrival_time = bus_arrival_time_data["json"].filter { |stop| stop["StopName"]["Zh_tw"] == "三民健康路口(西松高中)" }.first["EstimateTime"]
+    route = subscriber.route
+    route_dir = subscriber.route_dir
 
-        if stops_before_bus_arrival_time < 30
-          send_notif(subscriber.phone, "#{subscriber.route}路線公車即將到達博仁醫院")
-          break
-        end
+    loop do
+      bus_stops_data = fetch_bus_data(
+        url: "https://tdx.transportdata.tw/api/basic/v2/Bus/StopOfRoute/City/Taipei/#{route}?%24filter=Direction%20eq%20#{route_dir}&%24format=JSON",
+      )
+
+      if bus_stops_data["code"] = "200"
+        target_stop_seq = subscriber.target_stop
+
+        target_stop_name = bus_stops_data["json"]["Stops"].filter { |stop| stop["StopSequence"] == target_stop_seq }.first["StopName"]["Zh_tw"]
+        stops_before_target_stop_name = bus_stops_data["json"]["Stops"].filter { |stop| stop["StopSequence"] == target_stop_seq - 5 }.first["StopName"]["Zh_tw"]
+
+        bus_arrival_time_data = fetch_bus_data(
+          url: "https://tdx.transportdata.tw/api/basic/v2/Bus/EstimatedTimeOfArrival/City/Taipei/#{route}?%24filter=Direction%20eq%20#{route_dir}&%24format=JSON",
+        )
+
+        send_notif_if_stops_before_arrival_time_less_than_thirty_secs(subscriber.phone, route, target_stop_name, stops_before_target_stop_name, bus_arrival_time_data)
       end
 
       sleep(30)
@@ -19,13 +29,7 @@ class TrackBusJob < ApplicationJob
 
   private
 
-  def fetch_stops_before_bus_arrival_time(route, filter = "")
-    get_response(
-      url: "https://tdx.transportdata.tw/api/basic/v2/Bus/EstimatedTimeOfArrival/City/Taipei/#{route}?%24filter=#{filter}&%24format=JSON"
-    )
-  end
-
-  def get_response(url, options: {})
+  def fetch_bus_data(url:, options: {})
     uri = URI.parse(url)
     timestamp = get_gmt_timestamp
     hmac = encode_by_hmac_sha1(value = "x-date: " + timestamp)
@@ -64,6 +68,17 @@ class TrackBusJob < ApplicationJob
     JSON.parse(response.body.force_encoding("UTF-8"))
   rescue => e
     puts "JSON response parse error: #{e}"
+  end
+
+  def send_notif_if_stops_before_arrival_time_less_than_thirty_secs(phone, route, target_stop_name, stops_before_target_stop_name, response_data)
+    if response_data["code"] = "200"
+      stops_before_bus_arrival_time = response_data["json"].filter { |stop| stop["StopName"]["Zh_tw"] == stops_before_target_stop_name }.first["EstimateTime"]
+
+      if stops_before_bus_arrival_time < 30
+        send_notif(phone, "#{route}路線公車即將到達#{target_stop_name}")
+        break
+      end
+    end
   end
 
   def send_notif(phone, message)
